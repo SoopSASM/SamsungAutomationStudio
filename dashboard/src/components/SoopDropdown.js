22b3c2d44d303c3c69bb2485d65c57d7bf6245f6
zzunsik <ororwnstlr@naver.com>
2022-09-30 22:34:30 +0900

    [modify] send selected dropdown value

--- dashboard/src/components/SoopDropdown.js
+++ dashboard/src/components/SoopDropdown.js
@@ -4,34 +4,35 @@ import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
+import { sendMessage } from "../utils/socket";
 
 const DropdownContainer = styled.div`
   width: 200px;
 `;
 
-const SoopDropdown = () => {
+const SoopDropdown = ({ node }) => {
   const [option, setOption] = useState("");
 
   const exampleData = {
     label: "dropdown",
     tooltip: "dropdown node",
     placeholder: "Select option",
-    option: { option1: "value1", option2: "value2" },
+    option: node.options,
   };
   const entries = Object.entries(exampleData.option);
+  console.log(node.options);
 
-  const onChange = (e) => {
+  const onChange = e => {
     // FIXME: 현재 value로만 변경값이 저장되는데.. key는 어떻게 해야할지 허허
-    console.log(e.target.value);
+    // 일단 node.option에 value, label, type도 같이 보내게 했습니다. 나중에 꺼내쓸 수 있습니다.
     setOption(e.target.value);
+    sendMessage(node.id, { value: e.target.value });
   };
   return (
     <>
       <DropdownContainer>
         <FormControl fullWidth>
-          <InputLabel id="soop-dashboard-select-label">
-            {exampleData.label}
-          </InputLabel>
+          <InputLabel id="soop-dashboard-select-label">{exampleData.label}</InputLabel>
           <Select
             labelId="soop-dashboard-select-label"
             label={exampleData.placeholder}
@@ -40,8 +41,8 @@ const SoopDropdown = () => {
           >
             {entries.map((entry, idx) => {
               return (
-                <MenuItem key={idx} value={entry[1]}>
-                  {entry[1]}
+                <MenuItem key={idx} value={entry[1].label}>
+                  {entry[1].label}
                 </MenuItem>
               );
             })}
